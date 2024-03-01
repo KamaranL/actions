095dec1628aeb7dfa1b740634679140db0512ecc
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-01 17:26:30 -0500

    saved 2024-03-01 17:26:30

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-: ${INPUT_ALL:=false} ${INPUT_GIT:=false} ${INPUT_GITHUB:=false}
+# set global defaults
+: \
+    ${INPUT_ALL:=false} \
+    ${INPUT_GIT:=false} \
+    ${INPUT_GITHUB:=false}
 
 declare -A out
 
-out[name]=kam
+outputs=(
+    git
+    github
+    path
+)
 
-OUTPUT_GITHUB="{"
-OUTPUT_GIT="{"
-OUTPUT_PATH="{"
+for name in "${outputs[@]}"; do out[name]="{"; done
 
 [[ $INPUT_GITHUB =~ timezone-set ]] || $INPUT_ALL && {
-    # echo "->check(github.timezone-set)"
-    [ "$(</etc/timezone)" != "America/New_York" ] && val="false"
-    # echo "$(<'${{ steps.file.outputs.github }}') \"timezone-set\": ${val:-true}," >'${{ steps.file.outputs.github }}'
-    OUTPUT_GITHUB+=" \"timezone-set\": ${val:-true},"
+    echo "->check(github.timezone-set)"
+    [ "$(</etc/timezone)" != "America/New_York" ] && val=false
+    out[github]+=" \"timezone-set\": ${val:-true},"
 } && unset val
 
-echo "github=${OUTPUT_GITHUB%,*} }" >>"$GITHUB_OUTPUT"
-
-echo "${out[name]}"
+for name in "${outputs[@]}"; do
+    echo "$name=${out[name]%,*}" >>"$GITHUB_OUTPUT"
+done
 
 exit 0

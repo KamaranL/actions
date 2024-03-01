368a1526d9db783071fc4fd309ece5c17c17b672
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-01 13:48:14 -0500

    saved 2024-03-01 13:48:14

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-FILES=(
-    github
-    git
-    path
-)
-JSON_TEMP="$RUNNER_TEMP/.json"
-[ ! -d "$JSON_TEMP" ] && mkdir -p "$JSON_TEMP"
-for file in "${FILES[@]}"; do
-    JSON="$JSON_TEMP/$file"
-    echo "{" >"$JSON"
-    echo "$file=$JSON" >>"$GITHUB_OUTPUT"
-    echo "::debug::>open($file)"
-done
-echo "files=${FILES[@]}" >>"$GITHUB_OUTPUT"
-echo "json_temp=$JSON_TEMP" >>"$GITHUB_OUTPUT"
+# FILES=(
+#     github
+#     git
+#     path
+# )
+# JSON_TEMP="$RUNNER_TEMP/.json"
+# [ ! -d "$JSON_TEMP" ] && mkdir -p "$JSON_TEMP"
+# for file in "${FILES[@]}"; do
+#     JSON="$JSON_TEMP/$file"
+#     echo "{" >"$JSON"
+#     echo "$file=$JSON" >>"$GITHUB_OUTPUT"
+#     echo "::debug::>open($file)"
+# done
+# echo "files=${FILES[@]}" >>"$GITHUB_OUTPUT"
+# echo "json_temp=$JSON_TEMP" >>"$GITHUB_OUTPUT"
+
+echo hello from entrypoint

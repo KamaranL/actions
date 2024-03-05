ddfb73d1c7111a394124c1bde5bf7e6c748306bb
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-05 16:32:26 -0500

    saved 2024-03-05 16:32:26

--- setup/ci/main.sh
+++ setup/ci/main.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+if [ "${GITHUB_BASE_REF:-\$GITHUB_BASE_REF}" == main ] &&
+    [[ ${GITHUB_HEAD_REF:-\$GITHUB_HEAD_REF} =~ ^dev(elop)?(ment)?$ ]]; then
+    echo "is-prerelease=false" >>"$GITHUB_OUTPUT"
+fi
--- setup/ci/pre.sh
+++ setup/ci/pre.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-[ "$GITHUB_EVENT_NAME" != pull_request ] && {
-    echo "::error::$ACTION_REPOSITORY can only run on \"pull_request\""
-    echo "::error::$GITHUB_ACTION_REPOSITORY c@n only run on \"pull_request\""
-    exit 2
-}
+# [ "$GITHUB_EVENT_NAME" != pull_request ] && {
+#     echo "::error::$ACTION_REPOSITORY can only run on \"pull_request\""
+#     exit 2
+# }
+
+if git describe --tags --abbrev=0 >/dev/null 2>&1 &&
+    gh release view --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1; then
+    echo "override-config=$(
+        cat <<DOC
+next-version=1.0.0
+DOC
+    )" >>"$GITHUB_OUTPUT"
+fi

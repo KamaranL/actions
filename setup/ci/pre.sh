5e3e54363103f1951f0099b7aa8bba32185c2261
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-05 14:33:37 -0500

    saved 2024-03-05 14:33:37

--- setup/ci/pre.sh
+++ setup/ci/pre.sh
@@ -2,5 +2,6 @@
 
 [ "$GITHUB_EVENT_NAME" != pull_request ] && {
     echo "::error::$ACTION_REPOSITORY can only run on \"pull_request\""
+    echo "::error::$GITHUB_ACTION_REPOSITORY c@n only run on \"pull_request\""
     exit 2
 }

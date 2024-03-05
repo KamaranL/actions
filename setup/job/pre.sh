d8a50f87629dee1f3e49455d0750a9e60cf8967c
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-05 16:43:09 -0500

    saved 2024-03-05 16:43:09

--- setup/job/pre.sh
+++ setup/job/pre.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+INPUT_CHECKOUT=($INPUT_CHECKOUT)
+
 [ "$(</etc/timezone)" != "America/New_York" ] &&
     echo "timezone-set=false" >>"$GITHUB_OUTPUT"
 
@@ -8,3 +10,12 @@
 
 ! git status >/dev/null 2>&1 &&
     echo "checked-out=false" >>"$GITHUB_OUTPUT"
+
+
+echo "${#INPUT_CHECKOUT[@]}"
+echo "${INPUT_CHECKOUT[1]}"
+
+
+# for input in "${INPUT_CHECKOUT[@]}"; do
+#     echo "$input"
+# done

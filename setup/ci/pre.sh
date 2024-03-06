822d94cadbc9879944d98e7df55faca33673110c
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-06 08:32:52 -0500

    saved 2024-03-06 08:32:52

--- setup/ci/pre.sh
+++ setup/ci/pre.sh
@@ -19,7 +19,10 @@ if git describe --tags --abbrev=0 >/dev/null 2>&1 &&
 fi
 {
     echo "gitversion-execute_overrideConfig<<EOF"
-    echo next-version=1.0.0
+    cat <<DOC
+next-version=1.0.0
+mode: Mainline
+DOC
     echo EOF
 } >>"$GITHUB_OUTPUT"
 # echo "gitversion-execute_overrideConfig<<EOF"

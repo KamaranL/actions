627ebc34a9050db51c0e092488d8726ec7918a01
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-06 08:11:16 -0500

    saved 2024-03-06 08:11:16

--- setup/ci/main.sh
+++ setup/ci/main.sh
@@ -2,5 +2,6 @@
 
 if [ "${GITHUB_BASE_REF:-\$GITHUB_BASE_REF}" == main ] &&
     [[ ${GITHUB_HEAD_REF:-\$GITHUB_HEAD_REF} =~ ^dev(elop)?(ment)?$ ]]; then
-    echo "is-prerelease=false" >>"$GITHUB_OUTPUT"
+    # echo "is-prerelease=false" >>"$GITHUB_OUTPUT"
+    echo not prerelease
 fi
--- setup/ci/pre.sh
+++ setup/ci/pre.sh
@@ -7,7 +7,7 @@
 
 if git describe --tags --abbrev=0 >/dev/null 2>&1 &&
     gh release view --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1; then
-    echo "override-config=$(
+    echo "gitversion-execute_overrideConfig=$(
         cat <<DOC
 next-version=1.0.0
 DOC

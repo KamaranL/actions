040be8ad99bb4e62b04633d8f032510ae3ede1f1
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-06 14:27:48 -0500

    saved 2024-03-06 14:27:48

--- setup/ci/main.sh
+++ setup/ci/main.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+echo "::group::Configuring CI..."
+
+CI_VERSION="$GitVersion_MajorMinorPatch"
+CI_SOURCE_BRANCH="${GITHUB_HEAD_REF:-\$GITHUB_HEAD_REF}"
+
 if [ "${GITHUB_BASE_REF:-\$GITHUB_BASE_REF}" == main ] &&
     [[ ${GITHUB_HEAD_REF:-\$GITHUB_HEAD_REF} =~ ^dev(elop)?(ment)?$ ]]; then
-    # echo "is-prerelease=false" >>"$GITHUB_OUTPUT"
-    echo not prerelease
+    PRERELEASE=false
+else
+    PRERELEASE=true
 fi
 
+$PRERELEASE && {
+    echo "Appending prerelease tag (alpha)"
 
-echo running main
+    CI_VERSION+="alpha"
+
+    # check for files that are commonly found in nuget packages
+    NU_FILES=($(find . -type f \( \
+        -name '*.sln' -o \
+        -name '*.ps1' -o \
+        -name '*.psd1' -o \
+        -name 'nuget.config' \)))
+
+    # append period in prerelease tag if not a nuget package
+    ((!${#NU_FILES[@]})) &&
+        CI_VERSION+="."
+
+    CI_VERSION+="$GitVersion_CommitsSinceVersionSource"
+}
+
+echo "::endgroup::"
--- setup/identity/main.sh
+++ setup/identity/main.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo "::group::Configuring Git..."
+
 SSH_TEMP="$RUNNER_TEMP/.ssh"
 [ ! -d "$SSH_TEMP" ] && mkdir -p "$SSH_TEMP"
 echo "$SSH_ID" >"$SSH_TEMP/id_ed25519"
 echo "$SSH_ID_PUB" >"$SSH_TEMP/id_ed25519.pub"
 echo "${SSH_ID_PUB##* } ${SSH_ID_PUB% *}" >"$SSH_TEMP/allowed_signers"
 chmod 0600 "$SSH_TEMP/id_ed25519"
-
-echo "Configuring Git..."
 
 git config --global user.name "GitHub Actions"
 git config --global user.email "bot@kamaranl.vip"
@@ -19,3 +19,5 @@ git config --global tag.gpgsign true
 git config --global push.followtags true
 git config --global push.default "upstream"
 sed 's/^/\t/' <(git config --list --global)
+
+echo "::endgroup::"

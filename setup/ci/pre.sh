7e46983db0703cd050acbdfd254293f74ae6ea5b
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-06 08:51:37 -0500

    saved 2024-03-06 08:51:37

--- setup/ci/pre.sh
+++ setup/ci/pre.sh
@@ -5,26 +5,7 @@
 #     exit 2
 # }
 
-if git describe --tags --abbrev=0 >/dev/null 2>&1 &&
-    gh release view --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1; then
-    :
-    # {
-    #     echo "gitversion-execute_overrideConfig<<EOF"
-    #     echo next-version=1.0.0
-    #     echo EOF
-    # } >>"$GITHUB_OUTPUT"
-    # echo "gitversion-execute_overrideConfig<<EOF"
-    # echo next-version=1.0.0
-    # echo EOF >>"$GITHUB_OUTPUT"
+if ! git describe --tags --abbrev=0 >/dev/null 2>&1 &&
+    ! gh release view --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1; then
+    echo "gitversion-execute_overrideConfig=next-version=1.0.0" >>"$GITHUB_OUTPUT"
 fi
-{
-    echo "gitversion-execute_overrideConfig<<EOF"
-    cat <<DOC
-next-version=1.0.0
-mode: Mainline
-DOC
-    echo EOF
-} >>"$GITHUB_OUTPUT"
-# echo "gitversion-execute_overrideConfig<<EOF"
-# echo next-version=1.0.0
-# echo EOF >>"$GITHUB_OUTPUT"

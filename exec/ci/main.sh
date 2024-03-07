1ac6cc6b2f7d3926d32bf31172229a5d4380029c
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-07 08:11:19 -0500

    saved 2024-03-07 08:11:19

--- exec/ci/main.sh
+++ exec/ci/main.sh
@@ -3,8 +3,8 @@
 echo "::group::Integrating code base..."
 
 echo "- Amending commit"
-! git commit -a --amend --no-edit --date-now 2>&1 && {
-    echo "::error::There was a problem with amending the latest changes"
+! git commit -a --amend --no-edit --date=now 2>&1 && {
+    echo "::error::There was a problem with amending the latest changes."
     exit 1
 }
 
@@ -14,13 +14,13 @@ CI_TAG="v$CI_VERSION"
 
 echo "- Creating tag: $CI_TAG"
 ! git tag -a "$CI_TAG" -m "$CI_TAG" "$CI_SHA" 2>&1 && {
-    echo "::error::There was a problem with creating tag \"$CI_TAG\""
+    echo "::error::There was a problem with creating tag \"$CI_TAG\"."
     exit 1
 }
 
 echo "- Pushing changes to origin: $CI_ORIGIN"
 ! git push origin "$CI_ORIGIN" --force-with-lease 2>&1 && {
-    echo "::error::There was a problem with pushing changes to origin"
+    echo "::error::There was a problem with pushing changes to origin."
     exit 1
 }
 
--- exec/ci/pre.sh
+++ exec/ci/pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # [ "$GITHUB_EVENT_NAME" != pull_request ] && {
-#     echo "::error::$ACTION_REPOSITORY can only run on \"pull_request\""
+#     echo "::error::$ACTION_REPOSITORY can only run on \"pull_request\"."
 #     exit 1
 # }

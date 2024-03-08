2890b41a2320c3bc4c299dc978e3961238da68e1
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-08 07:54:54 -0500

    saved 2024-03-08 07:54:54

--- exec/cd/release/main.sh
+++ exec/cd/release/main.sh
@@ -2,17 +2,17 @@
 
 echo "::group::Delivering GitHub Release"
 
-ARGS="release create $TAG --latest --generate-notes --verify-tag"
+ARGS="release create $CI_TAG --latest --generate-notes --verify-tag"
 
 [ ! -z "$INPUT_ASSETS" ] &&
     ARGS+=" $INPUT_ASSETS/*"
 
 ! gh "$ARGS" 2>&1 && {
-    echo "::error::There was a problem creating a release for \"$TAG\"."
+    echo "::error::There was a problem creating a release for \"$CI_TAG\"."
     exit 1
 }
 
-echo "Release \"$TAG\" can be found at https://github.com/$GITHUB_REPOSITORY/releases/latest."
+echo "Release \"$CI_TAG\" can be found at https://github.com/$GITHUB_REPOSITORY/releases/latest."
 
 echo "::endgroup::"
 
--- exec/cd/release/pre.sh
+++ exec/cd/release/pre.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-TAG="$(gh api /repos/$GITHUB_REPOSITORY/tags --jq '.[0].name')"
+CI_TAG="$(gh api /repos/$GITHUB_REPOSITORY/tags --jq '.[0].name')"
 
 # check tag existence
-[ ! -z "$TAG" ] && {
+[ ! -z "$CI_TAG" ] && {
     echo "::error::No tag is available for release."
     exit 1
 }
 
 # check release existence
-gh release view "$TAG" --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1 && {
-    echo "::error::A release already exists for \"$TAG\"."
+gh release view "$CI_TAG" --repo "$GITHUB_REPOSITORY" >/dev/null 2>&1 && {
+    echo "::error::A release already exists for \"$CI_TAG\"."
     exit 1
 }
 
-echo "tag=$TAG" >>"$GITHUB_OUTPUT"
+echo "ci-tag=$CI_TAG" >>"$GITHUB_OUTPUT"
 
 exit 0

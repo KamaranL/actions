59b4c72b0c5358800292ee7ab136981d4d6d0512
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-08 07:41:11 -0500

    saved 2024-03-08 07:41:11

--- exec/cd/release/main.sh
+++ exec/cd/release/main.sh
@@ -7,11 +7,11 @@ ARGS="release create $TAG --latest --generate-notes --verify-tag"
 [ ! -z "$INPUT_ASSETS" ] &&
     ARGS+=" $INPUT_ASSETS/*"
 
-gh "$ARGS" 2>&1 && {
-    echo "Release \"$TAG\" can be found at https://github.com/$GITHUB_REPOSITORY/releases/latest."
-} || {
+! gh "$ARGS" 2>&1 && {
     echo "::error::There was a problem creating a release for \"$TAG\"."
     exit 1
 }
+
+echo "Release \"$TAG\" can be found at https://github.com/$GITHUB_REPOSITORY/releases/latest."
 
 echo "::endgroup::"

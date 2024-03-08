3db9a27150832546d3250ceebb6756d779a87b15
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-08 07:56:59 -0500

    saved 2024-03-08 07:56:59

--- exec/cd/release/main.sh
+++ exec/cd/release/main.sh
@@ -2,12 +2,12 @@
 
 echo "::group::Delivering GitHub Release"
 
-CMD="release create $CI_TAG --latest --generate-notes --verify-tag"
+ARGS="release create $CI_TAG --latest --generate-notes --verify-tag"
 
 [ ! -z "$INPUT_ASSETS" ] &&
-    CMD+=" $INPUT_ASSETS/*"
+    ARGS+=" $INPUT_ASSETS/*"
 
-! gh $CMD 2>&1 && {
+! gh $ARGS 2>&1 && {
     echo "::error::There was a problem creating a release for \"$CI_TAG\"."
     exit 1
 }

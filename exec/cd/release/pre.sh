453746ee162f8152245c583014dd183295c54413
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-15 10:58:38 -0400

    saved 2024-03-15 10:58:38

--- exec/cd/release/pre.sh
+++ exec/cd/release/pre.sh
@@ -2,9 +2,11 @@
 
 # CI_TAG="$(gh api /repos/$GITHUB_REPOSITORY/tags --jq '.[0].name')" #old method
 
+# get base branch from pr that triggered workflow
+PR_NUM="$(echo -e "$WORKFLOW_REF" | grep -oE '[0-9]{1,}')"
+BASE_REF="$(gh pr view "$PR_NUM" --json baseRefName --jq .baseRefName)"
 CI_TAG="v$(curl -s -H "Authorization: Token $GH_TOKEN" \
-    -L https://raw.githubusercontent.com/$GITHUB_REPOSITORY/${WORKFLOW_RUN_BASE_REF:-HEAD}/VERSION.txt)"
-# https://raw.githubusercontent.com/{repo}/{branch}/VERSION.txt
+    -L https://raw.githubusercontent.com/$GITHUB_REPOSITORY/${BASE_REF:-HEAD}/VERSION.txt)"
 
 # check tag existence
 [ -z "$CI_TAG" ] && {

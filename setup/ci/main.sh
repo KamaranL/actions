92475c50894ed98e71ada029f78b8ae053ab6e78
Kamaran Layne <42880993+KamaranL@users.noreply.github.com>
2024-03-28 09:16:26 -0400

    saved 2024-03-28 09:16:26

--- setup/ci/main.sh
+++ setup/ci/main.sh
@@ -4,23 +4,9 @@ echo ::group::Setting up CI...
 
 CI_VERSION="$GitVersion_MajorMinorPatch"
 
-# echo - Setting release type
-# if [ "$CI_TARGET_BRANCH" == main ]; then
-#     ! [[ $CI_SOURCE_BRANCH =~ ^dev(elop)?(ment)?$ ]] && {
-#         echo "::error::Branch \"$CI_SOURCE_BRANCH\" is not a development \
-# branch and therefore not allowed to merge into \"$CI_TARGET_BRANCH\". Merge \
-# \"$CI_SOURCE_BRANCH\" into a development branch first."
-#         exit 1
-#     }
-#     PRERELEASE=false
-# else
-#     PRERELEASE=true
-#     PRERELEASE_TAG=-alpha
-# fi
-
 $CI_PRERELEASE && {
-    echo - Appending prerelease tag: "$CI_PRERELEASE_LABEL"
-    CI_VERSION+="$CI_PRERELEASE_LABEL"
+    echo - Appending prerelease label: "$GitVersion_PreReleaseLabelWithDash"
+    CI_VERSION+="$GitVersion_PreReleaseLabelWithDash"
 
     # check for files that are commonly found in nuget packages
     NU_FILES=($(find . -type f \( \
@@ -33,8 +19,8 @@ $CI_PRERELEASE && {
         CI_VERSION+="." ||
         echo - Formatting prerelease version as nuget-compatible
 
-    echo - Appending commit count: "$GitVersion_CommitsSinceVersionSource"
-    CI_VERSION+="$GitVersion_CommitsSinceVersionSource"
+    echo - Appending prerelease number: "$GitVersion_PreReleaseNumber"
+    CI_VERSION+="$GitVersion_PreReleaseNumber"
 }
 
 echo "CI_VERSION=$CI_VERSION" >>"$GITHUB_ENV"

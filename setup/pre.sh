eb070bbf8cf51af3ecb7758cf628a36508993bbe
Kamaran Layne <kamaranl@kamaranl.vip>
2024-07-26 08:48:11 -0400

    added OP_SERVICE_ACCOUNT_TOKEN check

--- setup/pre.sh
+++ setup/pre.sh
@@ -4,6 +4,13 @@ echo ::group::bash "$0"
 
 declare -A out
 
+echo - Checking variable: OP_SERVICE_ACCOUNT_TOKEN
+[ -z "$OP_SERVICE_ACCOUNT_TOKEN" ] && {
+    echo ::error::OP_SERVICE_ACCOUNT_TOKEN not set.
+    echo ::endgroup::
+    exit 1
+}
+
 echo - Checking for \$GH_TOKEN
 [ -z "$GH_TOKEN" ] &&
     out[gh_token]=false
